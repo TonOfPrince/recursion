7e9207462d364e8e87ad7abe482cd5c914a4dc58
Brett Leibowitz <brettleibo@gmail.com>
2014-10-13 14:31:42 -0700

    getElem complete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -11,61 +11,22 @@ var getElementsByClassName = function(className){
   var nodeSearch = function(begin_node) {
  	if (!begin_node) {
 		begin_node = document.body;
-		//begin_node = $('body');
+		list.push(begin_node);
 	}
-	/*if (begin_node.classList && begin_node.classList.contains(className)) {
-			list.push(begin_node);
-	}*/
-	console.log(begin_node);
+	//console.log(begin_node);
 	var node = "";
-	for (var i = 0; i < begin_node.childNodes.length; i++) {
-		
+	for (var i = 0; i < begin_node.childNodes.length; i++) {	
 		node = begin_node.childNodes[i];
 		if  (node.hasChildNodes()) {
 			nodeSearch(node);
-			//arguments.callee(node);
 		}
-		/*if (node.className && node.className.indexOf(className) >= 0) {
-			list.push(node);
-		}*/
-		console.log(node);
+		//console.log(node);
 		if (node.classList && node.classList.contains(className)) {
 			list.push(node);
 		}
-		/*
-		if (node.hasAttribute && node.hasAttribute(className)) {
-			list.push(node);
-		}*/
 	}
   };
  nodeSearch();
   
   return list; 
-  /*
-  var node, first;
-  if (node == null && 
-  
-  if (document.hasAttribute && document.hasAttribute(className)) {
-	list.push(document)
-  }
-  
-  var childNodes = document.childNodes;
-  if (childNodes && childNodes.length > 0) {
-	for (var i = childNodes.length; i>0; i--) {
-		getElementsByClassName(className);
-	}
-  }
-  
-	
-  if (!node) {
-	return;
-  }
-  if (var node == null) {
-	node = document;
-  }
-  if (node.hasAttribute && node.hasAttribute(className)) {
-	list.push(node);
-  }
-  
-  */
 };
